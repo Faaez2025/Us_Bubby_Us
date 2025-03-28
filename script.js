c879ccd6f5b7763bb4ecce2b5d72a9fa10541f2b
Faaez2025 <faaez.khan16@gmail.com>
2025-03-28 19:36:33 +0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("revealButton").addEventListener("click", function() {
-    document.getElementById("hiddenMessage").classList.remove("hidden");
-    this.style.display = "none";
-});
